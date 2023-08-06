47dfc24d7bd500c05bdab5196fe8312f102a10a6
nanoubon <programmer_ubon@hotmail.com>
2023-08-06 21:00:14 +0700

    config ts

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+function total(a, b) {
+    return a + b;
+}
+console.log(total(100, 4000));
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+function total(a:number,b:number){
+    return a+b;
+}
+console.log(total(100,4000));
